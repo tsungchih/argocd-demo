56f6f842032ec298ec9c0c8e9dbb5a0b9bea4304
George T. C. Lai <george@innotech.me>
2022-02-11 19:07:07 +0800

    refactor(argocd): change values.yaml to values-non-ha.yaml
    change values.yaml to values-non-ha.yaml

--- sbin/helm-uninstall-argocd.sh
+++ sbin/helm-uninstall-argocd.sh
@@ -6,20 +6,7 @@ if [ -z ${ARGOCD_NS+x} ];then
   ARGOCD_NS='argocd'
 fi
 
-if [ -z ${1+x} ]; then
-  VALUES_FILE="${DIRNAME}/../helm/argo-cd/values.yaml"
-  echo "INFO: Using default values file 'helm/argo-cd/values.yaml'"
-else
-  if [ -f $1 ]; then
-    echo "INFO: Using values file $1"
-    VALUES_FILE=$1
-  else
-    echo "ERROR: No file exist $1"
-    exit 1
-  fi
-fi
-
-echo "INFO: Argocd will be installed on $ARGOCD_NS namespace with values file $VALUES_FILE"
+echo "INFO: Argocd will be uninstalled on $ARGOCD_NS namespace"
 echo -n "Do you want to proceed? [y/n]: "
 read ans
 if [ "$ans" == "y" ]; then
