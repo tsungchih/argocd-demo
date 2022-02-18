9f7c08e218aa51f3d1a04f648ae38c8af73851ba
George T. C. Lai <george@innotech.me>
2022-02-18 21:08:41 +0800

    refactor(shell): remove applicationset uninstallation from shell
    remove applicationset uninstallation from shell script

--- sbin/helm-uninstall-argocd.sh
+++ sbin/helm-uninstall-argocd.sh
@@ -11,8 +11,6 @@ echo -n "Do you want to proceed? [y/n]: "
 read ans
 if [ "$ans" == "y" ]; then
   helm uninstall argocd --namespace=argocd
-  # Install applicationset controller, will be deprecated starting v2.3 of Argo CD.
-  kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/applicationset/v0.3.0/manifests/install.yaml
 else
   echo "INFO: Exit without any action"
   exit 0
