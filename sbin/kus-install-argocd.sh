cab6a61a96963c64773aba9ea018579f65bce383
George T. C. Lai <tsungchih.hd@gmail.com>
2022-04-01 14:58:43 +0800

    feat(argocd): add kustomize deployment for argocd
    add kustomize deployment for argocd

--- sbin/kus-install-argocd.sh
+++ sbin/kus-install-argocd.sh
@@ -2,16 +2,11 @@
 
 DIRNAME=`dirname $0`
 ENV="${1:-non-ha}"
-VER="${2:-v2.2.5}"
 ENVDIR="${DIRNAME}/../kustomize/${ENV}"
 BASEDIR="${DIRNAME}/../kustomize/${ENV}/base"
 
 if [ ! -d ${ENVDIR} ]; then
   echo "Error: ${ENVDIR} not found. The installation can not continue."
-  exit 0
-elif [ ! -f ${BASEDIR}/argocd-${VER}-namespace-install.yaml ]; then
-  echo "Error: argocd-${VER}-namespace-install.yaml not found in the corresponding base dir."
-  echo "Please check if the version is correct."
   exit 0
 fi
 
@@ -19,8 +14,7 @@ echo "INFO: Argocd will be installed within argocd namespace."
 echo -n "Do you want to proceed? [y/n]: "
 read ans
 if [ "$ans" == "y" ]; then
-  kubectl apply -f ${BASEDIR}/argocd-${VER}-namespace-install.yaml
-  kustomize build ${ENVDIR} | kubectl apply -f -
+  kubectl kustomize ${ENVDIR} | kubectl apply -f -
 else
   echo "INFO: Exit without any action"
   exit 0
--- sbin/kus-uninstall-argocd.sh
+++ sbin/kus-uninstall-argocd.sh
@@ -2,16 +2,11 @@
 
 DIRNAME=`dirname $0`
 ENV="${1:-non-ha}"
-VER="${2:-v2.2.5}"
 ENVDIR="${DIRNAME}/../kustomize/${ENV}"
 BASEDIR="${DIRNAME}/../kustomize/${ENV}/base"
 
 if [ ! -d ${ENVDIR} ]; then
   echo "Error: ${ENVDIR} not found. The installation can not continue."
-  exit 0
-elif [ ! -f ${BASEDIR}/argocd-${VER}-namespace-install.yaml ]; then
-  echo "Error: argocd-${VER}-namespace-install.yaml not found in the corresponding base dir."
-  echo "Please check if the version is correct."
   exit 0
 fi
 
@@ -19,8 +14,7 @@ echo "INFO: Argocd will be uninstalled from argocd namespace."
 echo -n "Do you want to proceed? [y/n]: "
 read ans
 if [ "$ans" == "y" ]; then
-  kustomize build ${ENVDIR} | kubectl delete -f -
-  kubectl delete -f ${BASEDIR}/argocd-${VER}-namespace-install.yaml
+  kubectl kustomize ${ENVDIR} | kubectl delete -f -
 else
   echo "INFO: Exit without any action"
   exit 0
